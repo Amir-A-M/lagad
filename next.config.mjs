d8c39da1df5b2b3f2ed4298e8f58c911d18c4ed1
Amir Abbas Mousavi <mamyrbas@gmail.com>
2024-04-23 13:13:58 +0330

    http header

--- next.config.mjs
+++ next.config.mjs
@@ -4,19 +4,19 @@ const nextConfig = {
     config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
     return config;
   },
-  // async headers() {
-  //   return [
-  //     {
-  //       source: '/auth/reset-password/:token',
-  //       headers: [
-  //         {
-  //           key: 'Referrer-Policy',
-  //           value: 'no-referrer',
-  //         },
-  //       ],
-  //     },
-  //   ]
-  // },
+  async headers() {
+    return [
+      {
+        source: '/auth/reset-password/:token',
+        headers: [
+          {
+            key: 'Referrer-Policy',
+            value: 'no-referrer',
+          },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
